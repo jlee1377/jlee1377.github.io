22976467f656987c06900ec585693bb5c3e8a4f5
Jlee1377 <joseph.joe93@gmail.com>
2024-09-24 23:11:31 -0700

    update test
    removed accidental leftover code and added example code of twitch listener events

--- docs/test.js
+++ docs/test.js
@@ -50,200 +50,142 @@ async function getLiveFollowingStreams(){
   // .then((data) => renderStreams(data));
   console.log(res);
   console.log('getLiveFollowingStreams finished');
-=======
-// cd Documents/GitHub/jlee1377.github.io/docs
-// python3 -m http.server
 
-console.log("script initi");
 
-// Add addEventListener to textbox to allow it to be "Enter"able
-document.getElementById('twitchNameSearch').addEventListener(
-  "keypress", function(enter) {
-    // event.preventDefault();
-    if (enter.key === 'Enter') {
-      document.getElementById("submitButton").click();
+/*
+const urlParams = new URLSearchParams(window.location.search);
+const runtest = urlParams.get('test');
+
+  var options = {
+    width: 800,
+    height: 500,
+    channel: "monstercat",
+    layout: 'video-with-chat'
+  };
+  var player = new Twitch.Embed("test", options);
+
+  if (runtest) {
+    log('run test');
+    player.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+      log('Attempt volumne and unmute');
+      player.setVolume(0.1);
+      player.setMuted(false);
+    });
+  }
+
+  function log(txt, other) {
+    var sp = document.createElement('div');
+    sp.textContent = new Date().getTime() + ': ' + txt;
+
+    var t = other ? 'logb' : 'log';
+    document.getElementById(t).prepend(sp);
+  }
+
+  player.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+    log('The video is ready');
+  })
+  player.addEventListener(Twitch.Embed.VIDEO_PLAY, function() {
+    log('The video is playing');
+  })
+
+  document.getElementById('play').addEventListener('click', (e) => {
+    log('Attempt Play');
+    player.play();
+  });
+  document.getElementById('pause').addEventListener('click', (e) => {
+    log('Attempt Pause');
+    player.pause();
+  });
+  document.getElementById('volume').addEventListener('click', (e) => {
+    log('Attempt Volume: ' + document.getElementById('volume_value').value);
+    var val = parseFloat(document.getElementById('volume_value').value);
+    player.setVolume(val);
+  });
+  document.getElementById('volume_get').addEventListener('click', (e) => {
+    log('Attempt Get Volume');
+    log('Volume ' + player.getVolume());
+  });
+
+  document.getElementById('mute').addEventListener('click', (e) => {
+    log('Attempt Mute');
+    player.setMuted(true);
+  });
+  document.getElementById('unmute').addEventListener('click', (e) => {
+    log('Attempt UnMute');
+    player.setMuted(false);
+  });
+  document.getElementById('muted').addEventListener('click', (e) => {
+    log('Attempt Get muted');
+    log(player.getMuted());
+  });
+
+  setInterval(() => {
+    var status = '';
+
+    if (player.isPaused()) {
+      status += 'Paused ';
+    } else {
+      status += 'Playing ';
     }
+
+    if (player.getMuted()) {
+      status += 'Muted ';
+    } else {
+      status += 'UnMuted ';
+    }
+    status += 'Vol: ' + player.getVolume();
+
+    status += ' ' + player.getQuality();
+
+    document.getElementById('status').textContent = status;
+  }, 250);
+
+  document.getElementById('change').addEventListener('click', (e) => {
+    var channel = document.getElementById('channel').value;
+    log('Change channel ' + channel);
+    player.setChannel(channel);
+  });
+
+  document.getElementById('quality_get').addEventListener('click', (e) => {
+    log('Fetch Quality');
+    log(player.getQuality());
+  });
+  document.getElementById('qualities_get').addEventListener('click', (e) => {
+    log('Fetch Qualities');
+
+    var target = document.getElementById('quality');
+    target.textContent = '';
+
+    var qol = player.getQualities()
+    for (var x=0;x<qol.length;x++) {
+      var opt = document.createElement('option');
+      opt.value = qol[x].group;
+      opt.textContent = qol[x].name;
+      target.append(opt);
+    }
+  });
+  document.getElementById('quality_change').addEventListener('click', (e) => {
+    var target = document.getElementById('quality').value;
+    log('Set quality to ' + target);
+    player.setQuality(target);
+  });
+
+  var events = [
+    Twitch.Player.ENDED,
+    Twitch.Player.PAUSE,
+    Twitch.Player.PLAY,
+    Twitch.Player.PLAYBACK_BLOCKED,
+    Twitch.Player.PLAYING,
+    Twitch.Player.OFFLINE,
+    Twitch.Player.ONLINE,
+    Twitch.Player.READY
+  ]
+  for (var x=0;x<events.length;x++) {
+    quickBind(events[x]);
   }
-)
-
-// Keeping sticky header
-// console.log("testing sticky");
-window.onscroll = function() {stickyHeader()};
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-
-function stickyHeader() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-// IP add function to auto populate list with user input
-// document.getElementById('').
-// // API login
-// var access_token;
-// var token_type;
-// var authorizationObject;
-function twitchLogin(){
-  let clientId = config.clientID; //maybe put in global?
-  let clientSecret = config.clientSecret; //maybe put into global?
-  let authURL = 'https://id.twitch.tv/oauth2/authorize';
-  // let tokeURL = 'https://id.twitch.tv/oauth2/token';
-  let url = 'https://id.twitch.tv/oauth2/token?response_type=token&client_id='+clientId+'&&redirect_uri=http://localhost:8000/&scope=user%3Aread%3Afollows';
-
-  return fetch(url, { method: 'POST', })
-  .then((res) => res.json())
-  .then((data) => {
-    console.log('twitchLogin() completed. ' + data.token_type + ' ' + data.access_token);
-    console.log(data);
-    access_token = data.access_token;
-    token_type = data.token_type;
-    // let { access_token, expires_in, token_type } = authorizationObject;
-
-    return data});
-}
-async function getLiveFollowingStreams(){
-  const endpoint = 'https://api.twitch.tv/helix/streams';
-//   // try {
-    let authorizationObject = await twitchLogin();
-    let { access_token, expires_in, token_type } = authorizationObject;
-    // token_type first letter must be uppercase
-    token_type =
-    token_type.substring(0, 1).toUpperCase() +
-    token_type.substring(1, token_type.length);
-
-    let authorization = token_type + ' ' + access_token;
-
-    console.log(authorization);
-
-    let headers = {
-    authorization,
-    'Client-Id': clientId,
-    };
-
-    fetch(endpoint, {
-    headers,
-    })
-    .then((res) => res.json())
-    .then((data) => renderStreams(data));
-    console.log('getLiveFollowingStreams finished');
-}
-  // catch (err) {
-//   //   console.error('error: ' + err.message)
-//   // }
-// }
-// function renderStreams(data) {
-//     console.log(JSON.stringify(data));
-// }
-
-// Adding embed of stream
-function addEmbed(channelName){
-
-  // Clears submit box
-  document.getElementById('twitchNameSearch').value="";
-  // console.log("Cleared textbox?");
-
-  // if stream already exists or not
-  if (document.getElementById(channelName+'ID') !== null) {
-    // console.log("caught, refreshing")
-    document.getElementById(channelName + 'ID').remove();
-    addEmbed(channelName);
-  } else {
-    // console.log("not caught, creating")
-    // Creating stream name id string
-    var channelnameStreamDivID = channelName + 'ID'
-
-    // Setting variables for name ID to hold button name
-    // console.log("starting function for " + channelName);
-    var channelnameButtonID = channelName + "button"
-
-    // Creating containerLvl2 and set naming ID
-    // console.log("creating div");
-    var newTwitchEmbed = document.createElement('article');
-    newTwitchEmbed.id = channelnameStreamDivID;
-
-    // Creating button element to close
-    // console.log("creating button");
-    var closeButt = document.createElement('BUTTON');
-    closeButt.id=channelnameButtonID+"close";
-    closeButt.innerHTML="x";
-
-    // Creating button element for refresh
-    var refreshButt = document.createElement('BUTTON');
-    refreshButt.id=channelnameButtonID+"refresh";
-    refreshButt.innerHTML="re"
-
-    // Creating and setting embed containerLvl2 into containerLvl1
-    // console.log("embedding new div into container")
-    document.getElementById("twitch-container").appendChild(newTwitchEmbed);
-
-    // Creating embed
-    // console.log("creating embed");
-    var embed = new Twitch.Embed(channelnameStreamDivID, {
-      width: 368,
-      height: 207,
-      channel: channelName,
-      layout: "video",
-      parent: ["embed.example.com", "othersite.example.com"]
-    });
-
-    // console.log('testing channel name button id')
-    // console.log(channelnameButtonID);
-
-    // Adding button listener event
-    // console.log("creating button listener");
-    closeButt.addEventListener('click',() => {
-      // console.log('click');
-      document.getElementById(channelnameStreamDivID).remove();
-      // document.getElementById(channelnameButtonIDclose).remove();
-      // document.getElementById(channelnameButtonIDrefresh).remove();
-    })
-
-    refreshButt.addEventListener('click',()=>{
-      console.log("testing refresh");
-      document.getElementById(channelName + 'ID').remove();
-      addEmbed(channelName);
-    })
-
-    // Appending button into ContainerLvl2
-    // console.log("appending button");
-    document.getElementById(channelnameStreamDivID).appendChild(closeButt);
-    document.getElementById(channelnameStreamDivID).appendChild(refreshButt);
-    // console.log("attempting to add x");
-    // document.getElementById(channelnameButtonID).innerHTML="x";
-    // console.log("x appended");
-
-    // Setting embed settings
-    // console.log("creating embed listener");
-    embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-      var player = embed.getPlayer();
-      player.setQuality("160p");
-      player.setVolume("0.01");
-      player.play();
+  function quickBind(evt) {
+    player.addEventListener(evt, (e) => {
+      log('Captured ' + evt, true);
     });
   }
-}
-
-function refreshAll(){
-  var streamname ="";
-  // console.log("refreshing all ip. There are total of "+document.getElementById('twitch-container').childElementCount);
-  // TODO
-  for (let i = 0; i < document.getElementById('twitch-container').childElementCount; i++) {
-    // streamname= document.getElementById('twitch-container').firstElementChild.id.substring(0,document.getElementById('twitch-container').firstElementChild.id.length-2);
-    // console.log(streamname);
-    addEmbed(document.getElementById('twitch-container').firstElementChild.id.substring(0,document.getElementById('twitch-container').firstElementChild.id.length-2));
-  }
-}
-
-function clearAll() {
-  // console.log('clearing?');
-  // not getting through
-  while (document.getElementById('twitch-container').firstElementChild) {
-    document.getElementById('twitch-container').firstElementChild.remove();
-    // console.log('removed '+document.getElementById('twitch-container').firstElementChild.id)
-  }
-  // document.getElementById('twitch-container').clear();
-  // console.log('cleared?')
-}
+*/
