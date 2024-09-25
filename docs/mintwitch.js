0577798515d07d2e610d07add0068b3a1846ed88
Joseph Lee <joseph.joe93@gmail.com>
2024-09-24 22:05:35 -0700

    18.1 api , closing brnach  (#28)
    * remove dev-dash folder, not related to this project directly
    
    * uncommented related code
    
    attempting to reattempt api connection
    
    * commenting api for now to push dev branch back to hidden
    
    * uncommenting for api attempt
    
    * adding gitignore
    
    * uncommented api lines
    
    * followed button changed to live full list
    
    * creating separate test files
    
    to see if issue is due to using localhost on separate files.
    
    * commenting out twitch login button
    
    * comment out comment file useage, does not work on git
    
    * adding in react test, not important to api
    
    * issues with twitch api, closing branch until resolved

--- docs/mintwitch.js
+++ docs/mintwitch.js
@@ -1,5 +1,6 @@
-// cd Documents/GitHub/jlee1377.github.io/docs
-// python3 -m http.server
+/* 
+  python3 -m http.server
+*/
 
 console.log("script initi");
 
@@ -29,63 +30,60 @@ function stickyHeader() {
 
 // IP add function to auto populate list with user input
 // document.getElementById('').
-// // API login
-// var access_token;
-// var token_type;
-// var authorizationObject;
-// let clientId = config.clientID; //maybe put in global?
-// let clientSecret = config.clientSecret; //maybe put into global?
-// function twitchLogin(){
-//   let clientId = config.clientID; //maybe put in global?
-//   let clientSecret = config.clientSecret; //maybe put into global?
-//   let authURL = 'https://id.twitch.tv/oauth2/authorize';
-//   // let tokeURL = 'https://id.twitch.tv/oauth2/token';
-//   let url = 'https://id.twitch.tv/oauth2/token?response_type=token&client_id='+clientId+'&&redirect_uri=http://localhost:8000/&scope=user%3Aread%3Afollows';
 
-//   return fetch(url, { method: 'POST', })
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log('twitchLogin() completed. ' + data.token_type + ' ' + data.access_token);
-//     console.log(data);
-//     access_token = data.access_token;
-//     token_type = data.token_type;
-//     // let { access_token, expires_in, token_type } = authorizationObject;
+// API login
 
-//     return data});
-// }
-// async function getLiveFollowingStreams(){
-//   const endpoint = 'https://api.twitch.tv/helix/streams';
-// //   // try {
-//     let authorizationObject = await twitchLogin();
-//     let { access_token, expires_in, token_type } = authorizationObject;
-//     // token_type first letter must be uppercase
-//     token_type =
-//     token_type.substring(0, 1).toUpperCase() +
-//     token_type.substring(1, token_type.length);
+// This is only called through function getLiveFollowingStreams()
+function twitchLogin(){
+  let clientId = config.clientID; //maybe put in global?
+  let clientSecret = config.clientSecret; //maybe put into global?
 
-//     let authorization = token_type + ' ' + access_token;
+  // this is non-client request
+  // let url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
+  // this is implicit grant request
+  let url = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=http://localhost:8000&scopes=user%3Aread%3Afollows`;
+  // this is device code grant flow 
+  // let url = `https://id.twitch.tv/oauth2/device?client_id=${clientId}&scopes=user%3Aread%3Afollows`;
+  // console.log(url);
+  return fetch(url, { method: 'POST', })
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log('twitchLogin() completed. ' + data.token_type + ' ' + data.access_token);
+    console.log(data);
+    // access_token = data.access_token;
+    // token_type = data.token_type;
 
-//     console.log(authorization);
+    return data});
+}
 
-//     let headers = {
-//     authorization,
-//     'Client-Id': clientId,
-//     };
+// Call this function in html
+async function getLiveFollowingStreams(){
+  const endpoint = 'https://api.twitch.tv/helix/streams/followed';
 
-//     fetch(endpoint, {
-//     headers,
-//     })
-//     .then((res) => res.json())
-//     .then((data) => renderStreams(data));
-//     console.log('getLiveFollowingStreams finished');
-// }
-  // catch (err) {
-//   //   console.error('error: ' + err.message)
-//   // }
-// }
-// function renderStreams(data) {
-//     console.log(JSON.stringify(data));
-// }
+  let authorizationObject = await twitchLogin();
+  let { access_token, expires_in, token_type } = authorizationObject;
+
+  // [non-client request] token_type first letter must be uppercase
+  // token_type =
+  // token_type.substring(0, 1).toUpperCase() +
+  // token_type.substring(1, token_type.length);
+
+  // [non-client request]
+  // let authorization = `${token_type} ${access_token}`;
+  // console.log(authorization);
+
+  let headers = {
+    'user_id':authorizationObject.user_code,
+  };
+
+  fetch(endpoint, {
+    headers,
+  })
+  .then((res) => res.json())
+  // .then((data) => renderStreams(data));
+  console.log(res);
+  console.log('getLiveFollowingStreams finished');
+}
 
 // Adding embed of stream
 function addEmbed(channelName){
--- docs/test.js
+++ docs/test.js
@@ -1,3 +1,56 @@
+console.log("script initi");
+
+// IP add function to auto populate list with user input
+function twitchLogin(){
+  // let clientId = config.clientID;
+  // let clientSecret = config.clientSecret; 
+
+  // this is non-client request
+  // let url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
+  // this is implicit grant request
+  let url = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=http://localhost:8000&scopes=user%3Aread%3Afollows&response_type=token`;
+  // this is device code grant flow 
+  // let url = `https://id.twitch.tv/oauth2/device?client_id=${clientId}&scopes=user%3Aread%3Afollows`;
+  // console.log(url);
+  return fetch(url, { method: 'POST', })
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log('twitchLogin() completed. ' + data.token_type + ' ' + data.access_token);
+    console.log(data);
+    // access_token = data.access_token;
+    // token_type = data.token_type;
+
+    return data});
+}
+
+// Call this function in html
+async function getLiveFollowingStreams(){
+  const endpoint = 'https://api.twitch.tv/helix/streams/followed';
+
+  let authorizationObject = await twitchLogin();
+  let { access_token, expires_in, token_type } = authorizationObject;
+
+  // [non-client request] token_type first letter must be uppercase
+  // token_type =
+  // token_type.substring(0, 1).toUpperCase() +
+  // token_type.substring(1, token_type.length);
+
+  // [non-client request]
+  // let authorization = `${token_type} ${access_token}`;
+  // console.log(authorization);
+
+  let headers = {
+    'user_id':authorizationObject.user_code,
+  };
+
+  fetch(endpoint, {
+    headers,
+  })
+  .then((res) => res.json())
+  // .then((data) => renderStreams(data));
+  console.log(res);
+  console.log('getLiveFollowingStreams finished');
+=======
 // cd Documents/GitHub/jlee1377.github.io/docs
 // python3 -m http.server
 
